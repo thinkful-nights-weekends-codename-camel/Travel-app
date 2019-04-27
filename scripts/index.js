5c500c5ec6c2978948c960589bc07b07b36c9677
Emanuel Rouse <41143874+emanuelrouse@users.noreply.github.com>
2019-04-27 18:33:18 -0400

    session id function

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,20 @@
-console.log("test");
 const session = "";
 const skyScannerApiKey = "5893fbc347mshff1a803ba0f0e96p17e955jsn4e5d5e99ecad";
 const weatherApiKey = "";
+const sendParams = [
+  "inboundDate=2019-05-10",
+  "cabinClass=business",
+  "cabinClass=business",
+  "children=0",
+  "infants=0",
+  "country=US",
+  "currency=USD",
+  "locale=en-US",
+  "originPlace=PHL-sky",
+  "destinationPlace=YVR-sky",
+  "outboundDate=2019-05-01",
+  "adults=1"
+];
 
 function displayResults(responseJson) {
   console.log(responseJson);
@@ -16,11 +29,13 @@ function getSessionId() {
       "X-RapidAPI-Host":
         "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
       "Content-Type": "application/x-www-form-urlencoded"
-    })
+    }),
+    // Static send will change values dynamically later Not sure if obj properties have to be unique
+    body: sendParams.join("&")
   };
   fetch(url, options)
     .then(response => response.json())
-    .then(responseJson => console.log(responseJson)) // Change this to extract the session ID
+    .then(responseJson => displayResults(responseJson)) // Change this to extract the session ID
     .catch(error => alert(error.message));
   return sessionId;
 }
